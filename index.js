9439711cfc372ccdabfad69977a7d8e780209cd8
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-12-03 19:06:13 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.get("/hi",(req,res,next) => {res.send("res is res")})
+app.use("/", (req, res, next) => {
+  if (req.path === "/") {
+    res.write("hello ramya!");
+    res.write("roger!!");
+    res.end();
+  }
+  else {
+    next();
+  }
+});
+app.get("/hiramya", (req, res, next) => {
+  res.send("Hiiiii ramuuuuuuuuuuuuuu!");
+});
 
 app.listen(3000, () => {
   console.log("server is running");
